932a5c471932f4518ddc135acc1a4ed9181d39b8
Henryk Wollik <olafyoorsen@hotmail.com>
2016-07-23 21:23:50 +0200

    parse-data cleanup

--- parse-data.js
+++ parse-data.js
@@ -1,35 +1,310 @@
 const fs = require('fs');
+const path = require('path');
 const jsdoc_parse = require("jsdoc-parse");
+const colors = require('colors');
 
-//modules to parse
-const MODULES_TO_PARSE = [
-    'app',
-    'color',
-    'context-2d',
-    'context-2d-svg',
-    'context-gl'
-];
+const LOG_PREPARE_DATA = false;
+const PATH_OUT = './data';
+
+var modulesToParse = require('./parse-config.json').modules;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// PREPARE DATA
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+function prepareData(data){
+    for(var i = data.length - 1; i > -1; i--){
+        var item = data[i];
+
+        //foam-lib specific tags
+        var customTags = {};
+        for(var j = 0,l = item.customTags ? item.customTags.length : 0; j < l; ++j){
+            var tag = item.customTags[j];
+            customTags[tag.tag] = tag.value !== undefined ? tag.value : true;
+        }
+        var parsed = {
+            name : item.name,
+            type : item.kind,
+            description : item.description || '',
+            category : item.category || null,
+            examples : item.examples || []
+        };
+        var type = parsed.type;
+        var scope = item.scope;
+        //class
+        if(type === 'class'){
+            parsed.extends = item.augments || [];
+        }else{
+            //es5 setter/getter
+            if(customTags.custom_setter || customTags.custom_getter){
+                scope = customTags.custom_scope || scope;
+                parsed.name = item.memberof;
+                parsed.memberOf = customTags.custom_memberof;
+                parsed.type = customTags.custom_setter ? 'setter' : 'getter';
+                //object member
+            }else{
+                parsed.memberOf = item.memberof;
+            }
+            //scopes
+            parsed.scope = scope;
+            //function
+            if(type === 'constructor' ||
+                type === 'function' ||
+                type === 'setter' ||
+                type === 'getter'){
+                //function return type
+                var returns;
+                if(item.returns){
+                    returns = new Array(item.returns.length);
+                    for(var j = 0; j < returns.length; ++j){
+                        returns[j] = item.returns[j].type.names[0];
+                    }
+                    //no return type, default to 'void'
+                }else{
+                    returns = type === 'constructor' ? [parsed.memberOf] : ['void'];
+                }
+                parsed.returns = returns;
+                //function parameters
+                var params;
+                if(item.params){
+                    params = new Array(item.params.length);
+                    for(var j = 0; j < params.length; ++j){
+                        var param = item.params[j];
+                        params[j] = {
+                            name : param.name,
+                            description : param.description || '',
+                            optional : !!param.optional,
+                            types : param.type ? param.type.names : []
+                        }
+                    }
+                    //not parameters
+                }else{
+                    params = [];
+                }
+                parsed.params = params;
+            }
+            //constant
+            if(type === 'constant'){
+                if(item.isEnum){
+                    type = parsed.type = 'enum';
+                }
+            }
+        }
+        //enum or object entries
+        if(item.properties){
+            var properties  = item.properties;
+            var properties_ = new Array(properties.length);
+            for(var j = 0; j < properties.length; ++j){
+                var property = properties[j];
+                properties_[j] = {
+                    name : property.name,
+                    type : property.type.names,
+                    description : property.description
+                }
+            }
+            parsed.properties = properties_;
+        }
+        //correct type
+        if(parsed.type === 'member'){
+            parsed.type = item.type.names[0];
+        }
+        if(LOG_PREPARE_DATA){
+            console.log('entry',JSON.stringify(item));
+            console.log('---');
+            console.log('parsed',JSON.stringify(parsed));
+            console.log('--------------------------------------------------------------------------------------------');
+        }
+
+        data[i] = parsed;
+    }
+    return data;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// PARSE DATA
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+function parseData(data){
+    data = prepareData(data);
+
+    var classes = [];
+    var classKeyIndexMap = {};
+    var constants = [];
+    var enums = [];
+    var functions = [];
+
+    //get classes
+    for(var i = data.length - 1; i > -1; i--){
+        var item = data[i];
+        if(item.type !== 'class'){
+            continue;
+        }
+        classes.push({
+            name: item.name,
+            description: item.description,
+            properties: {
+                static : [],
+                instance : []
+            },
+            methods : {
+                static : [],
+                instance : []
+            }
+
+        });
+        classKeyIndexMap[item.name] = classes.length - 1;
+        data.splice(i,1);
+    }
+
+    //get class properties & methods
+    for(var name in classKeyIndexMap){
+        var class_ = classes[classKeyIndexMap[name]];
+        var class_properties = class_.properties;
+        var class_methods    = class_.methods;
+
+        for(var i = data.length - 1; i > -1; i--){
+            var item = data[i];
+            if(item.memberOf !== name){
+                continue;
+            }
+            switch(item.type){
+                case 'constructor':
+                    class_.constructor = {
+                        description : item.description,
+                        examples : item.examples,
+                        params : item.params
+                    };
+                    data.splice(i,1);
+                    break;
+                case 'function':
+                    class_methods[item.scope].push({
+                        name : item.name,
+                        description : item.description,
+                        category : item.category,
+                        examples : item.examples,
+                        returns : item.returns,
+                        params : item.params
+                    });
+                    data.splice(i,1);
+                    break;
+                case 'string':
+                case 'number':
+                    class_properties[item.scope].push({
+                        name : item.name,
+                        type : item.type,
+                        description : item.description,
+                        category : item.category,
+                        examples : item.examples
+                    });
+                    data.splice(i,1);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //get global constants, objects, enums, functions
+        for(var i = data.length - 1; i > -1; i--){
+            var item = data[i];
+
+            switch(item.type){
+                case 'constant':
+                    constants.push({
+                        name : item.name,
+                        description : item.description,
+                        category : item.category,
+                        examples : item.examples,
+                        properties : item.properties
+                    });
+                    data.splice(i,1);
+                    break;
+                case 'enum':
+                    enums.push({
+                        name : item.name,
+                        description : item.description,
+                        category : item.category,
+                        examples : item.examples,
+                        properties: item.properties
+                    });
+                    data.splice(i,1);
+                    break;
+                case 'function':
+                    functions.push({
+                        name: item.name,
+                        description: item.description,
+                        category : item.category,
+                        examples : item.examples,
+                        params : item.params,
+                        returns : item.returns
+                    });
+                    data.splice(i,1);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    if(data.length > 0){
+        console.log('Unhandled documentation:'.red);
+        console.log(JSON.stringify(data,null,'\t'));
+    }
+
+    return {
+        classes : classes,
+        constants : constants,
+        enums : enums,
+        functions : functions
+    };
+}
 
 function parse(module_string,cb){
-    var path = '../foam-' + module_string;
-    var files = fs.readdirSync(path);
+    var pathModule = '../' + module_string;
+    var files = fs.readdirSync(pathModule);
 
     for(var i = files.length - 1; i > -1; i--){
         var file = files[i];
         if(file.indexOf('.js') !== -1){
-            files[i] = path + '/' + file;
+            files[i] = pathModule + '/' + file;
             continue;
         }
         files.splice(i,1);
     }
 
-    var packagePath = path + '/package.json';
+    var packagePath = pathModule + '/package.json';
     var packageIndex = files.indexOf(packagePath);
 
     if(packageIndex === -1){
         throw new Error('No package.json');
     }
     files.splice(packageIndex,1);
+
+    var config_jsdoc_path = pathModule + '/jsdoc-config.json';
+    var config_jsdoc = null;
+
+    //jsdoc config
+    if(files.indexOf(config_jsdoc_path) !== -1){
+        //prefilter jsdoc config excludes
+        //TODO: cleanup
+        config_jsdoc = JSON.parse(fs.readFileSync(config_jsdoc_path));
+        if(config_jsdoc.source){
+            if(config_jsdoc.source.exclude){
+                var excludes = config_jsdoc.source.exclude;
+                for(var i = 0; i < excludes.length; ++i){
+                    var exclude = path.basename(excludes[i]);
+                    for(var j = files.length - 1; j > -1; j--){
+                        if(path.basename(files[j]) === exclude){
+                            files.splice(j,1);
+                        }
+                    }
+                }
+            }
+        }
+        files.splice(files.indexOf(config_jsdoc_path),1);
+    //no config available
+    }else{
+        config_jsdoc_path = null;
+    }
 
     var package_ = JSON.parse(fs.readFileSync(packagePath));
     var data = {
@@ -46,168 +321,21 @@ function parse(module_string,cb){
         });
     }
 
-    function parse_jsdoc_data(data){
-        data = JSON.parse(data);
+    var config = {
+        src : null,
+        private : true
+    };
 
-        var classes = [];
-        var classKeyIndexMap = {};
-        var global_constants = [];
-        var global_enums = [];
-        var global_objects = [];
-        var global_functions = [];
-
-        data.splice(i,1);
-
-        function parseFunction(data){
-            var returns = data.returns;
-            if(returns){
-                for(var i = 0;i < returns.length; ++i){
-                    returns[i] = returns[i].type.names[0];
-                }
-            } else {
-                returns = ['void'];
-            }
-            return {
-                name : data.name,
-                description : data.description,
-                params : data.params,
-                examples : data.examples,
-                return : returns
-            }
-        }
-
-        for(var i = data.length - 1; i > -1; i--){
-            var item = data[i];
-            //move custom tags to map
-            if(item.customTags){
-                var customTags = {};
-                for(var j = 0; j < item.customTags.length; ++j){
-                    var tag = item.customTags[j];
-                    customTags[tag.tag] = tag.value !== undefined ? tag.value : true;
-                }
-                item.customTags = customTags;
-            }
-            //extract classes
-            if(item.kind === 'class'){
-                var name = item.name;
-                if(!classKeyIndexMap[name]){
-                    classes.push({
-                        name : name,
-                        description : item.description,
-                        constructor : null,
-                        properties : {
-                            static : [],
-                            instance : []
-                        },
-                        functions : {
-                            static : [],
-                            instance : []
-                        }
-                    });
-                    classKeyIndexMap[name] = classes.length - 1;
-                    data.splice(i,1);
-                }
-            //extract non-class members
-            }else if(!item.memberof){
-                var name = item.name;
-                var description = item.description;
-                //global
-                if(item.kind === 'constant'){
-                    var type  = item.type.names[0];
-                    var typel = type.toLowerCase();
-                    //enum
-                    if(item.isEnum){
-                        var properties = item.properties;
-                        for(var j = 0; j < properties.length; ++j){
-                            var property = properties[j];
-                            property.type = property.type.names[0];
-                        }
-                        global_enums.push({
-                            name: name,
-                            description : description,
-                            type: type,
-                            properties: item.properties
-                        });
-                        data.splice(i,1);
-                    //object
-                    } else if(typel === 'object') {
-                        global_objects.push({
-                            name: name,
-                            description: description,
-                            properties: item.properties
-                        });
-                        data.splice(i,1);
-                    //primitive
-                    } else {
-
-                    }
-                }else if(item.kind === 'function'){
-                    global_functions.push(parseFunction(item));
-                    data.splice(i,1);
-                } else {
-
-                }
-            }
-        }
-        //extract class properties
-        for(var name in classKeyIndexMap){
-            var class_ = classes[classKeyIndexMap[name]];
-            var functions = class_.functions;
-            var properties = class_.properties;
-
-            for(var i = data.length - 1; i > -1; i--){
-                var item = data[i];
-                var isES5SetterGetter = item.customTags && item.customTags.custom_memberof === name;
-                if(item.memberof !== name && !isES5SetterGetter){
-                    continue;
-                }
-                switch(item.kind){
-                    case 'constructor':
-                        class_.constructor = parseFunction(item);
-                        break;
-                    case 'function':
-                        if(isES5SetterGetter){
-                            var scope = item.scope;
-                            var type = item.customTags.custom_setter ? 'set' : 'get';
-                            item.name = item.memberof; //actual function name
-                            item = parseFunction(item);
-                            item.type = type;
-                            functions[scope].push(item);
-                        }else{
-                            functions[item.scope].push(parseFunction(item));
-                        }
-                        break;
-                    default:
-                        properties[item.scope].push({
-                            name: item.name,
-                            type: item.type.names[0],
-                            description: item.description
-                        });
-                        break;
-                }
-                data.splice(i,1);
-            }
-        }
-
-        if(data.length > 0){
-            console.log('Unhandled documentation:');
-            console.log(JSON.stringify(data));
-        }
-
-        return {
-            classes : classes,
-            global_constants: global_constants,
-            global_enums: global_enums,
-            global_objects: global_objects,
-            global_functions: global_functions
-        };
+    if(config_jsdoc_path){
+        config.conf = config_jsdoc_path;
     }
 
     //parse module
     function parseModule(module,cb){
-        var stream = jsdoc_parse({src : module.path,private : false});
+        config.src = module.path;
+        var stream = jsdoc_parse(config);
         stream.on('data',function(data){
-            module.items = parse_jsdoc_data(data);
+            module.items = parseData(JSON.parse(data));
             cb();
         });
     }
@@ -226,4 +354,29 @@ function parse(module_string,cb){
 
     parseModule(data.modules[indexModule],onFileParsed);
 }
+
+console.log('foam-lib jsdoc documentation parse:'.yellow);
+
+//parse modules
+for(var i = 0; i < modulesToParse.length; ++i){
+    parse(modulesToParse[i],function(data){
+        console.log('└──',data.name);
+        for(var i = 0; i < data.modules.length; ++i){
+            console.log('   └──',path.basename(data.modules[i].path));
+        }
+
+        fs.writeFile(
+            path.join(PATH_OUT,data.name + '.json'),
+            JSON.stringify(data,null,'\t')
+        );
+    });
+}
+
+//write overview
+fs.writeFile(
+    path.join(PATH_OUT,'foam-modules.json'),
+    JSON.stringify({
+        modules : modulesToParse
+    },null,'\t')
+);
 
